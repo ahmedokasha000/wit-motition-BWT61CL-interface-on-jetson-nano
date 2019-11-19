c8a87fe91bf6380e7c0441a74bf4183a939b3b63
Ahmed Okasha <ahmednaserokasha@gmail.com>
2019-11-19 23:36:11 +0200

    Update imu_test.py

--- imu_test.py
+++ imu_test.py
@@ -20,24 +20,31 @@ try:
 
     counter=0
     start_byte=0
-    packet=bytearray([0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00])
+    Temp=0.0
+    acc=[0.0]*3
+    angVel=[0.0]*3
+    angle=[0.0]*3
+    packet= [0]*11
     while True:
-        if serial_port.inWaiting() > 0:
-            data = serial_port.read()
-            if (data == 'U') and (counter==0)  :
-                print "start byte detected"
-                start_byte=1
-            if start_byte==1 :
-                packet[counter]=data
-                counter+=1
-                if counter ==11 :
-                    counter =0
-                    start_byte=0
-                    print "packet received"
-                    print packet
-                    packet= bytearray([0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00])
-                
-            
+            if serial_port.inWaiting() > 0:
+                data =ord( serial_port.read())
+                #print(data)
+                if (data ==0x55) and (counter==0)  :
+                    print "start byte detected"
+                    start_byte=1
+                if start_byte==1 :
+                    packet[counter]=data
+                    counter+=1
+                    if counter ==11 :
+                        counter =0
+                        start_byte=0
+                        #print "packet received"
+                        #print packet
+                        decodePacket()
+                        packet= [0]*11
+
+
+
 
 
 
@@ -52,3 +59,27 @@ except Exception as exception_error:
 finally:
     serial_port.close()
     pass
+
+
+def decodePacket() :
+    global packet,angle,angVel,acc,Temp
+    if packet[1]==0x51 :
+          acc[0] = (packet [3] << 8 | packet [2]) / 32768.0 * 16
+          acc[1] = (packet [5] << 8 | packet [4]) / 32768.0 * 16
+          acc[2] = (packet [7] << 8 | packet [6]) / 32768.0 * 16
+          Temp =   (packet [9] << 8 | packet [8]) / 340.0 + 36.25
+    elif packet[1]==0x52:
+          angVel[0] = (packet [3] << 8 | packet [2]) / 32768.0 * 2000
+          angVel[1] = (packet [5] << 8 | packet [4]) / 32768.0 * 2000
+          angVel[2] = (packet [7] << 8 | packet [6]) / 32768.0 * 2000
+          Temp = (packet [9] << 8 | packet [8]) / 340.0 + 36.25        
+        
+    elif packet[1]==0x53:
+          angle[0] = (packet [3] << 8 | packet [2]) / 32768.0 * 180
+          angle[1] = (packet [5] << 8 | packet [4]) / 32768.0 * 180
+          angle[2] = (packet [7] << 8 | packet [6]) / 32768.0 * 180
+          Temp = (packet [9] << 8 | packet [8]) / 340.0 + 36.25
+          print("a :",acc,"w :",angVel,"angle :",angle,"Temp :",Temp)
+
+
+
